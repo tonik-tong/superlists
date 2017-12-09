13f6aaae134c81517304b8031d6e0984ccf2b88b
Tonik N K TONG <tongnaike@126.com>
2017-12-09 23:51:50 +0800

    First commit : First FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+if __name__ == "__main__":
+    # browser = webdriver.Firefox()
+    browser = webdriver.Chrome()
+    browser.get('http://localhost:8000')
+    assert 'Django' in browser.title
