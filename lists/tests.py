f8e3113f4c33571ad3d4515b9ff609d19d83da4c
Tonik N K TONG <tongnaike@126.com>
2017-12-12 00:23:52 +0800

    D:\pythonpractices\myproj\superlists>python manage.py startapp lists

---
+++ lists/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class SmokeTest(TestCase):
+
+    def test_bad_matches(self):
+        self.assertEqual(1+1, 3)
+
+# if __name__ == "__main__":
+#     TestCase.mai
+
